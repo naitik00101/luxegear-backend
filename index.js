1f49aa8cf214129f015ae4b8b43c1edc0b44e166
naitik00101 <naitikmorkar@gmail.com>
2026-02-24 12:12:58 +0530

    production api url

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running on port: ${PORT}`);
     console.log(`Database: ${process.env.MONGO_URI}`);
     console.log(`Environment: ${process.env.NODE_ENV || "development"}`);
 });
